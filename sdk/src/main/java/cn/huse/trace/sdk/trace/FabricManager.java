0733a8fc3ca8581966e5013d4219b180d40f69ce
huanxi <1355473748@qq.com>
2019-03-16 19:58:06 +0800

    路径修改

--- sdk/src/main/java/cn/huse/trace/sdk/trace/FabricManager.java
+++ sdk/src/main/java/cn/huse/trace/sdk/trace/FabricManager.java
@@ -69,7 +69,7 @@ public class FabricManager {
         FabricConfig config = new FabricConfig();
         config.setOrderers(getOrderers());
         config.setPeers(getPeers());
-        config.setChaincode(getChaincode(CHANNEL_NAME,CHAINCODE_NAME , "github.com/chaincode/chaincode_example02/go/", "1.0"));
+        config.setChaincode(getChaincode(CHANNEL_NAME, CHAINCODE_NAME, "github.com/chaincode/chaincode_example02/go/", "1.0"));
         config.setChannelArtifactsPath(getChannleArtifactsPath());
         config.setCryptoConfigPath(getCryptoConfigPath());
         return config;
@@ -128,7 +128,10 @@ public class FabricManager {
     private String getDirectory() {
        /* String directorys = FabricManager.class.getClassLoader().getResource("fabric").getFile();
         File directory = new File(directorys);*/
-        return FabricManager.class.getClassLoader().getResource("fabric").getPath().substring(1);
+        String path = FabricManager.class.getClassLoader().getResource("fabric").getPath();
+        String os = System.getProperty("os.name");
+        if (os.toLowerCase().startsWith("win")) path = path.substring(1);
+        return path;
     }
 
     /**
