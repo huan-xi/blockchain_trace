9658de64d2ac30295cee91df2c3864182f713e2a
huanxi <1355473748@qq.com>
2019-03-16 18:10:50 +0800

    路径修改

--- sdk/src/main/java/cn/huse/trace/sdk/trace/FabricConfig.java
+++ sdk/src/main/java/cn/huse/trace/sdk/trace/FabricConfig.java
@@ -39,13 +39,9 @@ public class FabricConfig {
 	 * @return D:/installSoft/apache-tomcat-9.0.0.M21-02/webapps/xxx/WEB-INF/classes/fabric/channel-artifacts/
 	 */
 	private String getChannlePath() {
-		String directorys = Objects.requireNonNull(FabricConfig.class.getClassLoader().getResource("fabric")).getFile();
-		log.debug("directorys = " + directorys);
+		String directorys = Objects.requireNonNull(FabricConfig.class.getClassLoader().getResource("fabric")).getPath();
 		File directory = new File(directorys);
-		log.debug("directory = " + directory.getPath());
-
 		return directory.getPath();
-		// return "src/main/resources/fabric/channel-artifacts/";
 	}
 
 	public Peers getPeers() {
