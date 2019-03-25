0f2d43af2ed3d70ee1cc381a1fa82060da00261d
huanxi <1355473748@qq.com>
2019-03-25 22:51:00 +0800

    区块链浏览器

---
+++ blokinfo/src/pages/Index.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="page">
+        <div class="block-table">
+            <Table border :columns="columns" :data="data">
+                <div slot="header" class="table-header">最新出块</div>
+                <div slot="footer" class="table-footer">
+                    <Page :total="count"/>
+                </div>
+            </Table>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .page {
+
+    }
+
+    .block-table {
+        margin: 30px;
+    }
+
+    .table-header {
+        padding-left: 20px;
+        background-color: #ddd;
+        color: #000;
+        font-size: 20px;
+    }
+
+    .table-footer {
+        text-align: center;
+    }
+</style>
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: "Index",
+        data() {
+            return {
+                columns: [
+                    {
+                        title: '高度',
+                        key: '',
+                        width: '100'
+                    },
+                    {
+                        title: '大小',
+                        key: 'age'
+                    },
+                    {
+                        title: '播报方',
+                        key: 'address'
+                    },
+                    {
+                        title: '时间',
+                        key: 'address'
+                    },
+                    {
+                        title: '数据hash',
+                        key: 'address'
+                    }
+                ],
+                data: [
+                    {
+                        name: '',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ],
+                count: 0
+            }
+        },
+        created() {
+            axios.get('/api/blockchain/info').then(e => {
+                console.log(e.data)
+                this.count = e.data.height
+            })
+        },
+        methods: {}
+    }
+</script>
+
---
+++ blokinfo/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    devServer: {
+        open: true,
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        //以上的ip和端口是我们本机的;下面为需要跨域的
+        proxy: {//配置跨域
+            '/api': {
+                target: 'http://111.230.251.119:8080',//这里后台的地址模拟的;应该填写你们真实的后台接口
+                ws: true,
+                changOrigin: true,//允许跨域
+                pathRewrite: {
+                    '^/api': ''//请求的时候使用这个api就可以
+                }
+            }
+
+        }
+    }
+}
