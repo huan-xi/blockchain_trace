71bdee95030190c931bd89c8b46456965c4e44d6
huanxi <1355473748@qq.com>
2019-05-01 10:10:41 +0800

    修复swagger

--- sdk/src/main/java/cn/huse/trace/TraceApplication.java
+++ sdk/src/main/java/cn/huse/trace/TraceApplication.java
@@ -1,21 +1,13 @@
 package cn.huse.trace;
 
-import cn.huse.trace.web.config.parsetoken.ParseTokenResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import java.util.List;
 
 @SpringBootApplication
-public class TraceApplication extends WebMvcConfigurationSupport {
+public class TraceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TraceApplication.class, args);
     }
-    @Override
-    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new ParseTokenResolver());
-    }
+
 }
--- sdk/src/main/java/cn/huse/trace/web/config/SwaggerConfig.java
+++ sdk/src/main/java/cn/huse/trace/web/config/SwaggerConfig.java
@@ -2,7 +2,8 @@ package cn.huse.trace.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,12 +11,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * swagger配置
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -25,6 +27,7 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("基于区块链的溯源系统RESTful APIs")
@@ -34,4 +37,12 @@ public class SwaggerConfig {
                 .version("1.0")
                 .build();
     }
-}
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) { // 解决静态资源无法访问
+         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+         // 解决swagger无法访问
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/"); }
+    }
