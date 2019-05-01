f13152b9f4c9c43c01ade103c07a4a41221949a8
huanxi <1355473748@qq.com>
2019-05-01 10:26:55 +0800

    修复swagger

--- sdk/src/main/java/cn/huse/trace/TraceApplication.java
+++ sdk/src/main/java/cn/huse/trace/TraceApplication.java
@@ -1,13 +1,21 @@
 package cn.huse.trace;
 
+import cn.huse.trace.web.config.parsetoken.ParseTokenResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @SpringBootApplication
-public class TraceApplication {
+public class TraceApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(TraceApplication.class, args);
     }
-
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ParseTokenResolver());
+    }
 }
